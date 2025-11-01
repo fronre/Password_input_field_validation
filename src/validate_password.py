fe299a3c0abb9373699f41947e69e3649454d30c
fronre <islamhala35@gmail.com>
2025-11-01 20:29:53 +0100

    Return False when less than 2 digit

--- src/validate_password.py
+++ src/validate_password.py
@@ -3,5 +3,11 @@ def validate_password(password: str) -> bool:
 
     if len(password) == 0:
         return False
+    if len(password) < 8:
+        return False
+
+    if sum(c.isdigit() for c in password) < 2:
+        return False
+
 
     return True
--- testes/test_validate_password.py
+++ testes/test_validate_password.py
@@ -1,4 +1,7 @@
 from src.validate_password import validate_password
 
-def test_password_is_empty():
-    password = "aaa"
+def test_password_has_less_than_two_digits():
+    password = "abcdefgiklm"
+    assert validate_password(password) == False
+    result = validate_password(password)
+
